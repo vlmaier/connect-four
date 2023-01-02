1ea954796e43f647b0d0d221163d509f9e3e3323
Vladas Maier <v.maier@dracoon.com>
2023-01-02 19:31:47 +0100

    Add "Standard input" topic

---
+++ Topics/Standard input/Read and print/src/Main.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val word1 = scanner.next()
+    val word2 = scanner.next()
+    val number1 = scanner.next()
+    val number2 = scanner.next()
+    println(word1)
+    println(word2)
+    println(number1)
+    println(number2)
+}

0ff08d80aca9cfe10fbaae629a4d202c7abd3560
Vladas Maier <v.maier@dracoon.com>
2023-01-04 17:24:41 +0100

    Add "Ranges" topic

---
+++ Topics/Ranges/Within both ranges/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val input = List(5) { readln().toInt() }
+    val firstRange = input[0]..input[1]
+    val secondRange = input[2]..input[3]
+    print(input[4] in firstRange && input[4] in secondRange)
+}

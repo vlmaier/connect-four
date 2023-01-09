3c502b548603c12dd98956e79abbab00e96d7393
Vladas Maier <v.maier@dracoon.com>
2023-01-09 20:19:44 +0100

    Solve "Generating passwords" for "Processing strings" topic

--- Topics/Processing strings/Generating passwords/src/Main.kt
+++ Topics/Processing strings/Generating passwords/src/Main.kt
@@ -1,3 +1,27 @@
 fun main() {
-    // write your code here    
+    val input = readln().split(" ").map { it.toInt() }
+    val a = input[0]
+    val b = input[1]
+    val c = input[2]
+    val n = input[3]
+    println(randomString(a, b, c, n))
+}
+
+fun randomString(a: Int, b: Int, c: Int, n: Int): String {
+    val uppercase = ('A'..'Z').toList()
+    val lowercase = ('a'..'z').toList()
+    val digits = ('0'..'9').toList()
+    val all = uppercase + lowercase + digits
+    val randomUppercase = List(a) { uppercase.random() }.joinToString("")
+    val randomLowercase = List(b) { lowercase.random() }.joinToString("")
+    val randomDigits = List(c) { digits.random() }.joinToString("")
+    val randomPadding = List(kotlin.math.abs(a + b + c - n)) { all.random() }.joinToString("")
+    val password = "$randomUppercase$randomLowercase$randomDigits$randomPadding".toCharArray()
+    // shuffle at least once for randomness
+    password.shuffle()
+    // find repeated characters
+    while ("(.)\\1".toRegex().find(String(password)) != null) {
+        password.shuffle()
+    }
+    return String(password)
 }

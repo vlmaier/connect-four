0c60283b62174dfae4ce7745f1466858f7997023
Vladas Maier <v.maier@dracoon.com>
2023-01-20 09:26:42 +0100

    Add "When expression" topic

---
+++ Topics/When expression/Shape/src/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var message = "You have chosen a "
+    when (readln().toInt()) {
+        1 -> message += "square"
+        2 -> message += "circle"
+        3 -> message += "triangle"
+        4 -> message += "rhombus"
+        else -> message = "There is no such shape!"
+    }
+    println(message)
+}

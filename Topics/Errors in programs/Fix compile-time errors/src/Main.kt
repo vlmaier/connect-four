4cb2329ee0a8af9d04264fdd1ba92fcc70997902
Vladas Maier <v.maier@dracoon.com>
2023-01-22 16:20:27 +0100

    Add "Errors in programs" topic

---
+++ Topics/Errors in programs/Fix compile-time errors/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val line1 = readln()
+    val line2 = readln()
+
+    println(line2)
+    println(line1)
+}
---
+++ Topics/Errors in programs/Fix the program/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val a = readln().toInt()
+    val b = readln().toInt()
+    val c = readln().toInt()
+    val sum = a + b + c
+    println(sum)
+}
